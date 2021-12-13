fe3500b6a4d83a58b4a9d80b38b93c8dcd654e57
azaf123 <ridhofaza1234@gmail.com>
2021-12-13 13:57:54 +0700

    pd changed to ps

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 from sklearn.feature_extraction.text import TfidfVectorizer
 import string
 import re
-import pandas as pd
+import pandas as ps
 
 app = Flask(__name__)
 
@@ -42,7 +42,7 @@ def home():
     
 
     #data di dapatkan dari web scrapping 
-    df = pd.read_csv("data\data_coffeeshop.csv")
+    df = ps.read_csv("data\data_coffeeshop.csv")
 
     #split dataset
     from sklearn.model_selection import train_test_split
